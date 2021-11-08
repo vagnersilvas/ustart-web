fe0df29a5d8770862e60d808fbf9cd49677ae03e
vagner da silva <vagnrdaia@gmail.com>
2021-11-07 22:01:46 -0300

    update cliente

--- src/app/clientes/clientes.module.ts
+++ src/app/clientes/clientes.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { ClientesRoutingModule } from './clientes-routing.module';
-import { ClientesComponent } from './pages/clientes/clientes.component';
 import { CadClienteComponent } from './pages/cad-cliente/cad-cliente.component';
 import { NzPageHeaderModule } from 'ng-zorro-antd/page-header';
 import { NzLayoutModule } from 'ng-zorro-antd/layout';
@@ -16,13 +15,20 @@ import { NzTableModule } from 'ng-zorro-antd/table';
 import { NzPaginationModule } from 'ng-zorro-antd/pagination';
 import { NzIconModule } from 'ng-zorro-antd/icon';
 import { NzSpinModule } from 'ng-zorro-antd/spin';
+<<<<<<< HEAD
 import { NzImageModule } from 'ng-zorro-antd/image';
 import { NzSelectModule } from 'ng-zorro-antd/select';
+=======
+import { NzSelectModule } from 'ng-zorro-antd/select';
+import { NzRadioModule } from 'ng-zorro-antd/radio';
+import { NgxMaskModule } from 'ngx-mask';
+import { ClienteComponent } from './pages/clientes/clientes.component';
+>>>>>>> 28f2b594553bc88e935d7bb11072522ee6bf918b
 
 
 @NgModule({
   declarations: [
-    ClientesComponent,
+    ClienteComponent,
     CadClienteComponent
   ],
   imports: [
@@ -41,8 +47,14 @@ import { NzSelectModule } from 'ng-zorro-antd/select';
     NzPaginationModule,
     NzIconModule,
     NzSpinModule,
+<<<<<<< HEAD
     NzImageModule,
     NzSelectModule
+=======
+    NzSelectModule,
+    NzRadioModule,
+    NgxMaskModule.forRoot()
+>>>>>>> 28f2b594553bc88e935d7bb11072522ee6bf918b
   ]
 })
 export class ClientesModule { }
